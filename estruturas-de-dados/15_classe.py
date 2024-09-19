29eb9a0f9aea82399c549ad63a1d25c354be88ab
Ellóra <160071572+olie-dc@users.noreply.github.com>
2024-09-18 22:03:52 -0300

    Add files via upload

---
+++ estruturas-de-dados/15_classe.py
@@ -0,0 +1,18 @@
+"""
+Classe é uma estrutura que represneta conjuntamente dados e algoritimos. Uma classe pode ser comparado com a uma "assadeira",
+com a qual se pode produzir diferentes tipos de "assados", variando os "ingredientes" (dados) da receita e o "modo de fazer"
+(algoritimos). Apesar dessas variações, todos os obejetos criados a partir de uma mesma classe terao sempre algumas
+caracteristicas comuns, impostos por ela.
+"""
+
+for match import pi  
+
+class FormaGeometrica:
+    """
+    Por convenção, nomes de casse seguem o formato PascalCase (Primeira letra de cada palavra em maiusculo). Uma classe pode ter. dentro de si
+    tanto dados quanto funções (estas, represnetando os algoritimos). Uma função especial, chamada __init__, é chamada sempre que se tenta criar um novo 
+    objeto a partir de uma classe. Essa função especial é chamada de CONSTRUTOR.
+    No contexto de classes e programação orientada a objetos:
+    ~> funções passam a ser chmadas METODOS; seu primeiro parametro é sempre self, representando o proprio objeto 
+    ~> variaveis passam a ser chamadas ATRIBUTOS
+    """
