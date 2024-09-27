1b24589282c42ba76254ad90e9d29a85f8661244
Ellóra <160071572+olie-dc@users.noreply.github.com>
2024-09-27 20:20:33 -0300

    Add files via upload

--- estruturas-de-dados/14_dicionarios.py
+++ estruturas-de-dados/14_dicionarios.py
@@ -8,7 +8,7 @@ NOMEADAS. Cada uma das posições nomeadas de um dicionário (ou
 seja, cada par de chave-valor) é chamada de PROPRIEDADE.
 """
 
-#Um dicionario representando os dados de uma pessoa
+# Um dicionário representando os dados de uma pessoa
 pessoa = {
     # "chave": valor
     "nome": "Orozimbo Oliveira Osório",
@@ -20,44 +20,48 @@ pessoa = {
     "filhos": ["Zeferino", "Adamastor", "Gercina"]
 }
 
-#Acessando o valor das propriedades 
-print(f"Nome:", pessoa["nome"])
-print(f"Idade:", pessoa["idade"])
-print(f"Aposentado?", pessoa["aposentado"])
+# Acessando o valor das propriedades
+print("Nome:", pessoa["nome"])
+print("Idade:", pessoa["idade"])
+print("Aposentado?", pessoa["aposentado"])
 
 print("-" * 80)
 
-#calculando o IMC (Indice de Massa Corporal)
+# Calculando o IMC (Índice de Massa Corporal)
+imc = pessoa["peso"] / pessoa["altura"] ** 2
 
-imc =  pessoa["peso"] / pessoa["altura"] ** 2
-
-#Nos f-strings delimitados por aspas duplas, é necessario usar aspas simples nos nomes das propriedades, e vice-versa
+# Nas f-strings delimitadas por aspas duplas, é necessário usar
+# aspas simples nos nomes das propriedades, e vice-versa
 print(f"O IMC de {pessoa['nome']} é {imc}.")
 
-######################################################################################################################################
+###############################################################
 
-#Usando dicionarios para represnetar formas geometricas 
+# Usando dicionários para representar formas geométricas
 
 forma1 = {
     "base": 7.5,
     "altura": 12,
-    "tipo": "R"      #Retangulo
+    "tipo": "R"     # Retângulo
 }
+
 forma2 = {
     "base": 6,
     "altura": 3,
-    "tipo": "T"      #Triangulo 
+    "tipo": "T"     # Triângulo
 }
+
 forma3 = {
     "base": 5,
     "altura": 3,
-    "tipo": "E"      #Elipse/ circulo      
+    "tipo": "E"     # Elipse/círculo
 }
+
 forma4 = {
     "legume": 10,
     "fruta": 6.2,
     "tipo": "T"
 }
+
 forma5 = {
     "base": "batata",
     "altura": False,
@@ -68,33 +72,29 @@ from math import pi
 
 def calc_area(forma):
     """
-    Função que calcula a area de uma forma geometrica, dados a base, a altura e tipo passados dentro de um dicionario
+    Função que calcula a área de uma forma geométrica, dados
+    a base, a altura e o tipo passados dentro de um dicionário
     """
     match forma["tipo"]:
-        case "R":           #Retangulo
-            return forma["base"] * forma["altura"] 
-        
-        case "T":           #Triangulo
+        case "R":       # Retângulo
+            return forma["base"] * forma["altura"]
+        case "T":       # Triângulo
             return forma["base"] * forma["altura"] / 2
-        
-        case "E":           #Elipse/ circulo
+        case "E":       # Elipse
             return (forma["base"] / 2) * (forma["altura"] / 2) * pi
+        case _:         # Forma inválida
+            return None
         
-        case _:             #Forma invalida
-            return None  
-
 print("-" * 80)
 
-#Testes coma função calc_area()
+# Testes com a função calc_area()
 
-print(f"Base: {forma1['base']}; altura: {forma1['altura']}; tipo: {forma1['tipo']}, area: {calc_area(forma1)}")
+print(f"Base: {forma1['base']}; altura: {forma1['altura']}; tipo: {forma1['tipo']}; área: {calc_area(forma1)}")
 
-print(f"Base: {forma2['base']}; altura: {forma2['altura']}; tipo: {forma2['tipo']}, area: {calc_area(forma2)}")
+print(f"Base: {forma2['base']}; altura: {forma2['altura']}; tipo: {forma2['tipo']}; área: {calc_area(forma2)}")
 
-print(f"Base: {forma3['base']}; altura: {forma3['altura']}; tipo: {forma3['tipo']}, area: {calc_area(forma3)}")
+print(f"Base: {forma3['base']}; altura: {forma3['altura']}; tipo: {forma3['tipo']}; área: {calc_area(forma3)}")
 
-#print(f"Base: {forma4['base']}; altura: {forma4['altura']}; tipo: {forma4['tipo']}, area: {calc_area(forma4)}") #vai dar erro
+# print(f"Base: {forma4['base']}; altura: {forma4['altura']}; tipo: {forma4['tipo']}; área: {calc_area(forma4)}")
 
-#print(f"Base: {forma5['base']}; altura: {forma5['altura']}; tipo: {forma5['tipo']}, area: {calc_area(forma5)}") #vai dar erro
-
-
+# print(f"Base: {forma5['base']}; altura: {forma5['altura']}; tipo: {forma5['tipo']}; área: {calc_area(forma5)}")
--- estruturas-de-dados/15_classe.py
+++ estruturas-de-dados/15_classe.py
@@ -1,18 +1,97 @@
 """
-Classe é uma estrutura que represneta conjuntamente dados e algoritimos. Uma classe pode ser comparado com a uma "assadeira",
-com a qual se pode produzir diferentes tipos de "assados", variando os "ingredientes" (dados) da receita e o "modo de fazer"
-(algoritimos). Apesar dessas variações, todos os obejetos criados a partir de uma mesma classe terao sempre algumas
-caracteristicas comuns, impostos por ela.
+Classe é uma estrutura que representa conjuntamente dados e
+algoritmos. Uma classe pode ser comparado a uma "assadeira",
+com a qual se pode produzir diferentes tipos de "assados",
+variando os "ingredientes" (dados) da receita e o "modo de
+fazer" (algoritmos). Apesar dessas variações, todos os objetos
+criados a partir de uma mesma classe terão sempre algumas
+características comuns, impostas por ela.
 """
-
-for match import pi  
+from math import pi
 
 class FormaGeometrica:
     """
-    Por convenção, nomes de casse seguem o formato PascalCase (Primeira letra de cada palavra em maiusculo). Uma classe pode ter. dentro de si
-    tanto dados quanto funções (estas, represnetando os algoritimos). Uma função especial, chamada __init__, é chamada sempre que se tenta criar um novo 
-    objeto a partir de uma classe. Essa função especial é chamada de CONSTRUTOR.
+    Por convenção, nomes de classe seguem o formato PascalCase
+    (primeira letra de cada palavra em maiúsculo).
+    Uma classe pode ter, dentro de si, tanto dados quanto funções
+    (estas, representando os algoritmos). Uma função especial,
+    chamada __init__, é chamada sempre que se tenta criar um
+    novo objeto a partir de uma classe. Essa função especial é
+    chamada de CONSTRUTOR.
     No contexto de classes e programação orientada a objetos:
-    ~> funções passam a ser chmadas METODOS; seu primeiro parametro é sempre self, representando o proprio objeto 
-    ~> variaveis passam a ser chamadas ATRIBUTOS
+    ~> funções passam a ser chamadas MÉTODOS; seu primeiro parâmetro
+       é sempre self, representando o próprio objeto
+    ~> variáveis passam a ser chamadas ATRIBUTOS
     """
+
+    def __init__(self, base, altura, tipo):
+        """ Método construtor """
+        self.set_base(base)
+        self.set_altura(altura)
+        self.set_tipo(tipo)
+
+    def set_base(self, val):
+        """ Método setter para o atributo __base """
+        # Valida se base é numérica e maior que zero
+        if type(val) not in [int, float] or val <= 0:
+            raise Exception("ERRO: 'base' deve ser numérica e maior que zero.")
+        # Sobreviveu à validação, armazena val no atributo
+        self.__base = val
+
+    def set_altura(self, val):
+        """ Método setter para o atributo __altura """
+        # Valida se altura é numérica e maior que zero
+        if type(val) not in [int, float] or val <= 0:
+            raise Exception("ERRO: 'altura' deve ser numérica e maior que zero")
+        # Sobreviveu à validação, armazena val no atributo
+        self.__altura = val
+
+    def set_tipo(self, val):
+        """ Método setter para o atributo __tipo """
+        # Valida o tipo de forma
+        if val not in ["R", "T", "E"]:
+            raise Exception("ERRO: 'tipo' deve ser 'R', 'T' ou 'E'.")
+        # Sobreviveu à validação, armazena val no atributo
+        self.__tipo = val
+
+    def get_base(self):
+        """Metodo getter para atributo __base"""
+        return self.__base
+    
+    def get_altura(self):
+        """Metodo getter para o atributo __altura"""
+        return self.__altura
+    
+    def get_tipo(self):
+        """Metodo getter para o atributo __tip  mnnn mn mn mn m,.no"""
+        return self.__tipo
+    
+    def calc_area(self):
+        if self.__tipo == "R":           #Retangulo
+            return self.__base * self.__altura
+        elif self.__tipo == "T":         #Triangulo
+            return self.__base * self.__base / 2
+        else:                            #Elipse/Circulo
+            return (self.__base / 2) * (self.__altura / 2) * pi
+        
+    def __str__(self):
+        """Provê uma reprsentação do objeto como string"""
+        return f"[FormaGeometrica], base: {self.__base}, altura: {self.__altura}, tipo: {self.__tipo}, area: {self.calc_area()}"
+
+
+###########################################################################
+
+# Criando uma forma geométrica a partir da classe
+forma1 = FormaGeometrica(20, 12.5, "R")
+
+# Nesse ponto, o objeto já existe, podemos modificar seus atributos
+#forma1.base = "tomate"
+#forma1.altura = -7
+#forma1.tipo = "W"
+
+forma1.set_base(4.5)
+forma1.set_altura(12)
+forma1.set_tipo("T")
+
+print(f"Dados da forma1: base = {forma1.get_base()}, altura = {forma1.get_altura()}, tipo = {forma1.get_tipo()}, area = {forma1.calc_area()}")
+
